688176bcd194b617332b1da3256e466dfd64d0a4
RaquelAguiar <raquelaguiar@id.uff.br>
2021-05-09 01:13:33 -0300

    cria a lista de imagens no jogo das figuras

--- lib/pages/jogo_figuras.dart
+++ lib/pages/jogo_figuras.dart
@@ -11,6 +11,27 @@ class Figuras extends StatelessWidget {
     );
   }
   _body(context) {
+
+    List<Image> _imagens = [
+      _img("assets/imagens/bicicleta.png"),
+      _img("assets/imagens/bola.png"),
+      _img("assets/imagens/boneca.png"),
+      _img("assets/imagens/cachorro.png"),
+      _img("assets/imagens/carro.png"),
+      _img("assets/imagens/casaco.png"),
+      _img("assets/imagens/hamburguer.png"),
+      _img("assets/imagens/livro.png"),
+      _img("assets/imagens/moto.png"),
+      _img("assets/imagens/peteca.png"),
+      _img("assets/imagens/radio.png"),
+      _img("assets/imagens/relógio.png"),
+      _img("assets/imagens/telefone.png"),
+      _img("assets/imagens/televisao.png"),
+      _img("assets/imagens/urso.png"),
+    ];
+
+    List<Image> imagensEmbaralhadas = _imagens..shuffle();
+
     Size size = MediaQuery.of(context).size;
     return Container(
       width: size.width,
@@ -18,9 +39,16 @@ class Figuras extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          _img("assets/imagens/urso.png"),
-          _img("assets/imagens/radio.png"),
-          _img("assets/imagens/televisao.png")
+          Expanded(flex: 5, child: Text("Memorize essas figuras: ",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.purple,
+                fontWeight: FontWeight.bold,
+              ))),
+          Expanded(flex: 1, child: Text(" ", style: TextStyle(color: Colors.white))),
+          Expanded(flex: 1, child: Text(" ", style: TextStyle(color: Colors.white))),
+          Expanded(flex: 1, child: Text(" ", style: TextStyle(color: Colors.white))),
         ],
       ),
     );
