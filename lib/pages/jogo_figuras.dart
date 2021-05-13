a2dce860073b2e65e2bcd2c64369360225ba6adb
RaquelAguiar <raquelaguiar@id.uff.br>
2021-05-13 00:01:25 -0300

    Modifica o estado da classe

--- lib/pages/jogo_figuras.dart
+++ lib/pages/jogo_figuras.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:memory_extreme_app/pages/instrucoes.dart';
+import 'package:memory_extreme_app/pages/matriz.dart';
 
-class Figuras extends StatelessWidget {
-  
-  List<String> _imagens = [
+class Figuras extends StatefulWidget {
+
+  List<String> imagens;
+
+  @override
+  _FigurasState createState() => _FigurasState();
+}
+
+class _FigurasState extends State<Figuras> {
+
+  List<String> imagens = [
     "assets/imagens/bicicleta.png",
     "assets/imagens/bola.png",
     "assets/imagens/boneca.png",
@@ -21,13 +30,6 @@ class Figuras extends StatelessWidget {
     "assets/imagens/urso.png",
   ];
 
-  get _imagensSelecionadas {
-    List<String> imagensEmbaralhadas = _imagens..shuffle();
-    List<String> _imagensSelecionadas = [...imagensEmbaralhadas].take(9).toList();
-    print("Aqui é o print das Imagens selecionadas $_imagensSelecionadas");
-    return _imagensSelecionadas;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,7 +41,17 @@ class Figuras extends StatelessWidget {
   }
   _body(context) {
 
-    List _foto = _imagensSelecionadas;
+    List<String> obterSelecionadas() {
+      List<String> imagensEmbaralhadas = imagens..shuffle();
+      List<String> imagensSelecionadas = imagensEmbaralhadas.take(9).toList();
+      return imagensSelecionadas;
+    }
+
+    List<String> sorteadas = [];
+    sorteadas = obterSelecionadas();
+    print("Sorteadas: $sorteadas");
+    List<String> embaralhadas = sorteadas..shuffle();
+    print("Embaralhadas: $embaralhadas");
 
     Size size = MediaQuery.of(context).size;
     return Container(
@@ -56,8 +68,8 @@ class Figuras extends StatelessWidget {
                 color: Colors.purple,
                 fontWeight: FontWeight.bold,
               ))),
-          Expanded(flex: 1, child: Matriz()._img(_foto[0])),
-          Expanded(flex: 1, child: Matriz()._img(_foto[1])),
+          Expanded(flex: 1, child: _img(embaralhadas[0])),
+          Expanded(flex: 1, child: _img(embaralhadas[1])),
           Expanded(flex: 0, child: _button(context, "Avançar")),
           Expanded(flex: 1, child: Text(" ", style: TextStyle(color: Colors.white))),
         ],
@@ -78,35 +90,31 @@ class Figuras extends StatelessWidget {
         color: Colors.purple,
         child: Text(text, style: TextStyle(color: Colors.white)),
         onPressed: () {
-          _onClickAvancar(context);
+          setState(() {
+            _onClickAvancar(context);
+          });
         }
     );
   }
 
   _onClickAvancar(context) {
-    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
-      return Matriz();
-    }));
+    Navigator.push(context, MaterialPageRoute(
+          builder: (context) => Imagens(imagens, imagens),
+        ));
   }
-
 }
 
 class OpcoesFiguras extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-      ),
+      appBar: AppBar(),
       body: _body(context),
-
     );
   }
 
   _body(context) {
-    Size size = MediaQuery
-        .of(context)
-        .size;
+    Size size = MediaQuery.of(context).size;
     return Container(
       padding: EdgeInsets.only(top: 40),
       width: size.width,
@@ -114,21 +122,30 @@ class OpcoesFiguras extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          Expanded(flex: 1, child: Text(
-              "O que você gostaria de fazer?",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 30,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-
-              ))),
-          Expanded(flex: 0, child: _button(context, "   Ir para o Jogo   ", () => _onClickNavigator(context, Figuras()))),
-          Expanded(flex: 0, child: _button(context, "Ir para Instruções", () => _onClickNavigator(context, figuras(context)))),
-          Expanded(flex: 3, child: Text(
-              " ",
-              style: TextStyle(fontSize: 200,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold))),
+          Expanded(
+              flex: 1,
+              child: Text("O que você gostaria de fazer?",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ))),
+          Expanded(
+              flex: 0,
+              child: _button(context, "   Ir para o Jogo   ",
+                  () => _onClickNavigator(context, Figuras()))),
+          Expanded(
+              flex: 0,
+              child: _button(context, "Ir para Instruções",
+                  () => _onClickNavigator(context, figuras(context)))),
+          Expanded(
+              flex: 3,
+              child: Text(" ",
+                  style: TextStyle(
+                      fontSize: 200,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold))),
         ],
       ),
     );
@@ -138,8 +155,7 @@ class OpcoesFiguras extends StatelessWidget {
     return RaisedButton(
         color: Colors.purple,
         child: Text(text, style: TextStyle(color: Colors.white)),
-        onPressed: onPressed
-    );
+        onPressed: onPressed);
   }
 
   void _onClickNavigator(BuildContext context, Widget home_page) {
@@ -150,68 +166,5 @@ class OpcoesFiguras extends StatelessWidget {
 
   Widget figuras(context) {
     return InstrucoesFiguras();
-
-  }
-
-}
-
-class Matriz extends StatelessWidget {
-
-  List _foto = Figuras()._imagensSelecionadas..shuffle();
-
-  List get foto => _foto;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _body(context),
-    );
-  }
-
-  _body(context) {
-
-    print("Aqui é o print da lista de fotos embaralhadas $_foto");
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      padding: EdgeInsets.all(2),
-      width: size.width,
-      color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Expanded(flex: 1, child: _img(_foto[0])),
-              Expanded(flex: 1, child: _img(_foto[1])),
-              Expanded(flex: 1, child: _img(_foto[2])),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Expanded(flex: 1, child: _img(_foto[3])),
-              Expanded(flex: 1, child: _img(_foto[4])),
-              Expanded(flex: 1, child: _img(_foto[5])),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Expanded(flex: 1, child: _img(_foto[6])),
-              Expanded(flex: 1, child: _img(_foto[7])),
-              Expanded(flex: 1, child: _img(_foto[8])),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-  _img(String img) {
-    return Image.asset(
-      img,
-      width: 100,
-      height: 100,
-    );
   }
 }
