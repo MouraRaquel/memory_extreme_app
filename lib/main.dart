4a9b666dda30cbe9d41b1f26c8a912d96a7e37c4
RaquelAguiar <raquelaguiar@id.uff.br>
2021-05-16 16:49:19 -0300

    Redireciona para Home Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:memory_extreme_app/home_page.dart';
 import 'package:memory_extreme_app/pages/login.dart';
 
 
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: Colors.black),
-      home: Login(),
+      home: HomePage(),
     );
   }
 }
