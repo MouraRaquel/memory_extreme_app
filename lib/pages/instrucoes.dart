1bc973fa4e10dd293e2bea8c221d9dcd11a9b841
RaquelAguiar <raquelaguiar@id.uff.br>
2021-05-21 19:04:42 -0300

    Cria o botão sair

--- lib/pages/instrucoes.dart
+++ lib/pages/instrucoes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:memory_extreme_app/home_page.dart';
 import 'package:memory_extreme_app/pages/jogo_figuras.dart';
 import 'package:memory_extreme_app/pages/jogo_palavras.dart';
 
@@ -18,7 +19,7 @@ class InstrucoesPalavras extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
 
     return Container(
-      padding: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.all(20),
       width: size.width,
       color: Colors.black,
       child: Column(
@@ -36,32 +37,58 @@ class InstrucoesPalavras extends StatelessWidget {
                   "memorizadas.",
                   textAlign: TextAlign.center,
                   style: TextStyle(
-                    fontSize: 35,
+                    fontSize: 25,
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
                   ))),
           Expanded(
               flex: 0, child: Text(" ", style: TextStyle(color: Colors.white))),
-          Expanded(flex: 0, child: _button(context, "Avançar")),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Expanded(flex: 0, child: _buttonAdvance(context, "Avançar")),
+              Expanded(flex: 0, child: _buttonExit(context, "Sair")),
+
+            ],
+          ),
           Expanded(
-              flex: 1, child: Text(" ", style: TextStyle(color: Colors.white))),
+              flex: 0, child: Text(" ", style: TextStyle(color: Colors.white))),
+          Expanded(
+              flex: 0, child: Text(" ", style: TextStyle(color: Colors.white))),
         ],
       ),
     );
   }
 
-  _button(context, String text) {
+  _buttonAdvance(context, String text) {
     return TextButton(
-        style: TextButton.styleFrom(backgroundColor: Colors.purple),
-        child: Text(text, style: TextStyle(color: Colors.white)),
+        style: TextButton.styleFrom(backgroundColor: Colors.purple, minimumSize: Size(150, 50)),
+        child: Text(text, style: TextStyle(color: Colors.white, fontSize: 30)),
         onPressed: () {
-          _onClickAvancar(context);
+          _onClickAdvance(context);
         });
   }
 
-  _onClickAvancar(context) {
+  _buttonExit(context, String text) {
+    return TextButton(
+        style: TextButton.styleFrom(
+            backgroundColor: Colors.purple, minimumSize: Size(150, 50)),
+        child: Text(text, style: TextStyle(color: Colors.white, fontSize: 30)),
+        onPressed: () {
+          _onClickExit(context);
+        });
+  }
+
+  _onClickAdvance(context) {
     Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
-      return Palavras();
+      return Figuras(9, 2);
+    }));
+  }
+
+  _onClickExit(context) {
+    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
+      return HomePage();
     }));
   }
 }
@@ -81,48 +108,73 @@ class InstrucoesFiguras extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
 
     return Container(
-      padding: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.all(20),
       width: size.width,
       color: Colors.black,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Expanded(
-              flex: 5,
-              child: Text(
-                  "O Jogo das Figuras funciona assim: Aparecerão algumas "
-                  "figuras, memorize-as. Depois, tente encontrá-las no "
-                  "meio de todas as figuras mostradas a seguir. Toque nas "
-                  "figuras que apareceram anteriormente.",
+        children: [
 
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 35,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ))),
+              Expanded(
+                  flex: 5,
+                  child: Text(
+                      "O Jogo das Figuras funciona assim: Aparecerão algumas "
+                      "figuras, memorize-as. Depois, tente encontrá-las no "
+                      "meio de todas as figuras mostradas a seguir. Toque nas "
+                      "figuras que apareceram anteriormente.",
+
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 32,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ))),
+              Expanded(
+                  flex: 0, child: Text(" ", style: TextStyle(color: Colors.white))),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+          Expanded(flex: 0, child: _buttonAdvance(context, "Avançar")),
+              Expanded(flex: 0, child: _buttonExit(context, "Sair")),
+
+            ],
+          ),
           Expanded(
               flex: 0, child: Text(" ", style: TextStyle(color: Colors.white))),
-          Expanded(flex: 0, child: _button(context, "Avançar")),
-          Expanded(
-              flex: 1, child: Text(" ", style: TextStyle(color: Colors.white))),
         ],
       ),
     );
   }
 
-  _button(context, String text) {
+  _buttonAdvance(context, String text) {
     return TextButton(
-        style: TextButton.styleFrom(backgroundColor: Colors.purple),
-        child: Text(text, style: TextStyle(color: Colors.white)),
+        style: TextButton.styleFrom(backgroundColor: Colors.purple, minimumSize: Size(150, 50)),
+        child: Text(text, style: TextStyle(color: Colors.white, fontSize: 30)),
         onPressed: () {
-          _onClickAvancar(context);
+          _onClickAdvance(context);
         });
   }
 
-  _onClickAvancar(context) {
+  _buttonExit(context, String text) {
+    return TextButton(
+        style: TextButton.styleFrom(
+            backgroundColor: Colors.purple, minimumSize: Size(150, 50)),
+        child: Text(text, style: TextStyle(color: Colors.white, fontSize: 30)),
+        onPressed: () {
+          _onClickExit(context);
+        });
+  }
+
+  _onClickAdvance(context) {
     Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
       return Figuras(9, 2);
+    }));
+  }
+
+  _onClickExit(context) {
+    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
+      return HomePage();
     }));
   }
 }
