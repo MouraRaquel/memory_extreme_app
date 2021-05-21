e52b166ecddaf23da16921b6f5946985765dcf59
RaquelAguiar <raquelaguiar@id.uff.br>
2021-05-20 22:57:42 -0300

    Cria diálogo de retorno da página

--- lib/pages/jogo_palavras.dart
+++ lib/pages/jogo_palavras.dart
@@ -3,6 +3,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:memory_extreme_app/home_page.dart';
+import 'package:memory_extreme_app/main.dart';
+import 'package:memory_extreme_app/pages/drawer_list.dart';
 import 'package:memory_extreme_app/pages/instrucoes.dart';
 
 class Palavras extends StatefulWidget {
@@ -101,12 +103,15 @@ class _PalavrasState extends State<Palavras> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Palavras"),
-      ),
-      body: _body(context),
-    );
+    return WillPopScope(
+        onWillPop: _onBackPressed,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Palavras"),
+          ),
+          body: _body(context),
+          drawer: DrawerList(),
+        ));
   }
 
   _body(context) {
@@ -146,7 +151,7 @@ class _PalavrasState extends State<Palavras> {
                     color: Colors.white,
                     fontWeight: FontWeight.bold)),
           ),
-          Expanded(flex: 0, child: _button(context, "Avançar")),
+          Expanded(flex: 1, child: _button(context, "Avançar")),
           Expanded(
               flex: 1, child: Text(" ", style: TextStyle(color: Colors.white))),
         ],
@@ -168,6 +173,38 @@ class _PalavrasState extends State<Palavras> {
       return Categoria(listaCategoriaEscolhida);
     }));
   }
+
+  Future<bool> _onBackPressed() {
+    return showDialog(
+          context: context,
+          builder: (context) => new AlertDialog(
+            title: new Text(
+              'Tem certeza que gostaria de sair do Jogo?',
+              textAlign: TextAlign.center,
+            ),
+            content: new Text(
+              'Você irá voltar para a página inicial',
+              textAlign: TextAlign.center,
+            ),
+            actions: <Widget>[
+              new TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: new Text('Não'),
+              ),
+              new TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MyApp()),
+                  );
+                },
+                child: new Text('Sim'),
+              ),
+            ],
+          ),
+        ) ??
+        false;
+  }
 }
 
 class Categoria extends StatefulWidget {
@@ -182,11 +219,23 @@ class Categoria extends StatefulWidget {
 class _CategoriaState extends State<Categoria> {
   @override
   Widget build(BuildContext context) {
-    String clicadas;
-    List<String> palavrasSelecionadas;
-    palavrasSelecionadas = widget.categoria..shuffle();
+    return WillPopScope(
+        onWillPop: _onBackPressed,
+        child: Scaffold(
+          body: _body(context),
+        ));
+  }
+
+  _body(context) {
+    String clicada;
+    List<String> palavrasDaMatriz;
+    print(widget.categoria);
+    print(palavrasDaMatriz);
+    palavrasDaMatriz = widget.categoria..shuffle();
+    print("Embaralhadas: $palavrasDaMatriz");
+
     var _estiloBotao = (ElevatedButton.styleFrom(
-        primary: Colors.purple, minimumSize: Size(115, 50)));
+        primary: Colors.purple, minimumSize: Size(110, 50)));
     var _fontes = TextStyle(
         fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white);
 
@@ -250,8 +299,13 @@ class _CategoriaState extends State<Categoria> {
       );
     }
 
+    String apresentadas = widget.categoria[0];
+    int contador = 0;
+    List<String> clicadas = [];
     _verificarPalavras(BuildContext context) {
-      if (clicadas == palavrasSelecionadas) {
+      if (apresentadas.contains(clicada)) {
+        clicadas.add(clicada);
+        print("Clicadas: $clicadas");
         _palavrasCorretas(context);
         print("Contém");
       } else {
@@ -269,96 +323,131 @@ class _CategoriaState extends State<Categoria> {
         children: <Widget>[
           Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.max,
               children: <Widget>[
                 TextButton(
                     style: _estiloBotao,
                     onPressed: () {
-                      clicadas = palavrasSelecionadas[0];
-                      print(clicadas);
+                      clicada = palavrasDaMatriz[0];
+                      print(clicada);
                       _verificarPalavras(context);
                     },
-                    child: Text(palavrasSelecionadas[0],
+                    child: Text(palavrasDaMatriz[0],
                         textAlign: TextAlign.center, style: _fontes)),
                 TextButton(
                     style: _estiloBotao,
                     onPressed: () {
-                      clicadas = palavrasSelecionadas[1];
+                      clicada = palavrasDaMatriz[1];
                       _verificarPalavras(context);
                     },
-                    child: Text(palavrasSelecionadas[1],
+                    child: Text(palavrasDaMatriz[1],
                         textAlign: TextAlign.center, style: _fontes)),
                 TextButton(
                     style: _estiloBotao,
                     onPressed: () {
-                      clicadas = palavrasSelecionadas[2];
+                      clicada = palavrasDaMatriz[2];
                       _verificarPalavras(context);
                     },
-                    child: Text(palavrasSelecionadas[2],
+                    child: Text(palavrasDaMatriz[2],
                         textAlign: TextAlign.center, style: _fontes)),
               ]),
           Text("", style: TextStyle(color: Colors.white)),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
             children: <Widget>[
               TextButton(
                   style: _estiloBotao,
                   onPressed: () {
-                    clicadas = palavrasSelecionadas[3];
+                    clicada = palavrasDaMatriz[3];
                     _verificarPalavras(context);
                   },
-                  child: Text(palavrasSelecionadas[3],
+                  child: Text(palavrasDaMatriz[3],
                       textAlign: TextAlign.center, style: _fontes)),
               TextButton(
                   style: _estiloBotao,
                   onPressed: () {
-                    clicadas = palavrasSelecionadas[4];
+                    clicada = palavrasDaMatriz[4];
                     _verificarPalavras(context);
                   },
-                  child: Text(palavrasSelecionadas[4],
+                  child: Text(palavrasDaMatriz[4],
                       textAlign: TextAlign.center, style: _fontes)),
               TextButton(
                   style: _estiloBotao,
                   onPressed: () {
-                    clicadas = palavrasSelecionadas[5];
+                    clicada = palavrasDaMatriz[5];
                     _verificarPalavras(context);
                   },
-                  child: Text(palavrasSelecionadas[5],
+                  child: Text(palavrasDaMatriz[5],
                       textAlign: TextAlign.center, style: _fontes)),
             ],
           ),
           Text("", style: TextStyle(color: Colors.white)),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
             children: <Widget>[
               TextButton(
                   style: _estiloBotao,
                   onPressed: () {
-                    clicadas = palavrasSelecionadas[6];
+                    clicada = palavrasDaMatriz[6];
                     _verificarPalavras(context);
                   },
-                  child: Text(palavrasSelecionadas[6],
+                  child: Text(palavrasDaMatriz[6],
                       textAlign: TextAlign.center, style: _fontes)),
               TextButton(
                   style: _estiloBotao,
                   onPressed: () {
-                    clicadas = palavrasSelecionadas[7];
+                    clicada = palavrasDaMatriz[7];
                     _verificarPalavras(context);
                   },
-                  child: Text(palavrasSelecionadas[7],
+                  child: Text(palavrasDaMatriz[7],
                       textAlign: TextAlign.center, style: _fontes)),
               TextButton(
                   style: _estiloBotao,
                   onPressed: () {
-                    clicadas = palavrasSelecionadas[8];
+                    clicada = palavrasDaMatriz[8];
                     _verificarPalavras(context);
                   },
-                  child: Text(palavrasSelecionadas[8],
+                  child: Text(palavrasDaMatriz[8],
                       textAlign: TextAlign.center, style: _fontes)),
             ],
           ),
         ],
       ),
     );
+  }
+
+  Future<bool> _onBackPressed() {
+    return showDialog(
+          context: context,
+          builder: (context) => new AlertDialog(
+            title: new Text(
+              'Tem certeza que gostaria de sair do Jogo?',
+              textAlign: TextAlign.center,
+            ),
+            content: new Text(
+              'Você irá voltar para a página inicial',
+              textAlign: TextAlign.center,
+            ),
+            actions: <Widget>[
+              new TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: new Text('Não'),
+              ),
+              new TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MyApp()),
+                  );
+                },
+                child: new Text('Sim'),
+              ),
+            ],
+          ),
+        ) ??
+        false;
   }
 
   void _onClickNavigator(BuildContext context, Widget homePage) {
